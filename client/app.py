4db40882b99e3a347ddc606c11602a2c1d514f8a
Daniel Ohayon <danielohayon2019@gmail.com>
2024-04-13 12:11:05 +0300

    added skeleton for client

--- client/app.py
+++ client/app.py
@@ -1,1 +1,30 @@
 import constants as c
+import sys
+
+
+def handle():
+    # common logic
+    # wait for broadcast
+    # change state
+    # open TCP
+    # receive question
+    pass
+
+
+def handle_bot():
+    # send answer bot
+    handle()
+
+
+def handle_human():
+    # send answer human
+    handle()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        if sys.argv[1] == 'bot':
+            # be bot
+            handle_bot()
+    # be human
+    handle_human()
